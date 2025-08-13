fedbc62ea20ceef3844b85481043a8bbbb110301
Stepkoff <stepan.rudiak95@gmail.com>
2025-08-13 11:08:00 +0200

    Prettier init

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-import { Button } from "./components/ui/button";
+import { Button } from './components/ui/button';
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
-import { Input } from "./components/ui/input";
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from '@/components/ui/accordion';
+import { Input } from './components/ui/input';
 
 export const App = () => {
-  return <div>
-
-
-    <div className="mb-8 mt-8">
-      <Button>Submit</Button>
-    </div>
-    <div className="">
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-          </AccordionItem>
-      </Accordion>
-    </div>
-    <div>
-      <Input/>
-    </div>
-  </div>;
+    return (
+        <div>
+            <div className="mb-8 mt-8">
+                <Button>Submit</Button>
+            </div>
+            <div className="">
+                <Accordion type="single" collapsible>
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+            </div>
+            <div className=" p-8 border-4 mx-4">
+                <Input />
+            </div>
+        </div>
+    );
 };
