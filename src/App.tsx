e35f6e30d06fcdc3b0bc3ae6fd6e386e6f82c06c
Stepkoff <stepan.rudiak95@gmail.com>
2025-08-13 10:57:17 +0200

    Tailwind + shadcn Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,31 @@
+import { Button } from "./components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { Input } from "./components/ui/input";
+
 export const App = () => {
-  return <div>Hello</div>;
+  return <div>
+
+
+    <div className="mb-8 mt-8">
+      <Button>Submit</Button>
+    </div>
+    <div className="">
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+          </AccordionItem>
+      </Accordion>
+    </div>
+    <div>
+      <Input/>
+    </div>
+  </div>;
 };
