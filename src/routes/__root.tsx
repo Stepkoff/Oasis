ae69a0b82be8cdfc1469db952c0152b835ecaeb1
Stepkoff <stepan.rudiak95@gmail.com>
2025-08-13 14:43:50 +0200

    Tanstack router added

---
+++ src/routes/__root.tsx
@@ -0,0 +1,45 @@
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { ThemeToggle } from '@/components/theme-toggle';
+import { ThemeProvider } from '@/components/theme-provider';
+import { useLayoutEffect } from 'react';
+import { setWindowInnerHeightIntoCssVariable } from '@/lib/utils';
+
+const RootComponent = () => {
+    useLayoutEffect(() => {
+        // 100vh bug fix for ios
+        window.addEventListener('resize', setWindowInnerHeightIntoCssVariable);
+        setWindowInnerHeightIntoCssVariable();
+        return () => {
+            window.removeEventListener(
+                'resize',
+                setWindowInnerHeightIntoCssVariable,
+            );
+        };
+    }, []);
+
+    return (
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+            <div className="min-h-custom-screen">
+                <div className="p-2 flex gap-2">
+                    <Link to="/" className="[&.active]:font-bold">
+                        Home
+                    </Link>{' '}
+                    <Link to="/about" className="[&.active]:font-bold">
+                        About
+                    </Link>
+                </div>
+                <div>
+                    <ThemeToggle />
+                </div>
+                <hr />
+                <Outlet />
+                <TanStackRouterDevtools />
+            </div>
+        </ThemeProvider>
+    );
+};
+
+export const Route = createRootRoute({
+    component: () => <RootComponent />,
+});
